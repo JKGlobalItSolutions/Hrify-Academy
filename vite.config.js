5ecfaef2e6f588dc52152d3050c498d20e13d824
kirubakaran182204 <vijayarajkirubakaran@gmail.com>
2026-06-30 14:19:41 +0530

    /

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Hrify-Academy/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
